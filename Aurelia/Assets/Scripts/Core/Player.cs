de8f0776e583ea4f19f4471b6bf5b82b27c2ea83
ChrisBrian <97772501+ChrisBrian@users.noreply.github.com>
2022-02-20 00:29:35 -0500

    Save System
    Save System prototype implementation

--- Aurelia/Assets/Scripts/Core/Player.cs
+++ Aurelia/Assets/Scripts/Core/Player.cs
@@ -128,4 +128,18 @@ public class Player : MonoBehaviour
             FindObjectOfType<GameManager>().Restart();
         }
     }
+
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+    
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+        Vector2 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        transform.position = position;
+    }
 }
